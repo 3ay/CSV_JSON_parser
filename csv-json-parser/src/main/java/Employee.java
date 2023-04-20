f7a03a51e24bf2b5176d547c194c1af4714a6065
3ay <lefrost.volkoff@yandex.ru>
2023-04-20 20:38:14 +0300

    add second task

--- csv-json-parser/src/main/java/Employee.java
+++ csv-json-parser/src/main/java/Employee.java
@@ -1,3 +1,4 @@
+
 public class Employee {
     public long id;
     public String firstName;
---
+++ xml-json-parser/src/main/java/MainXml.java
@@ -0,0 +1,54 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainXml {
+    public static List<Employee> parseXML(String fileName) {
+        List<Employee> list = new ArrayList<>();
+
+        try {
+            File xmlFile = new File(fileName);
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nodeList = doc.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+                    Employee employee = new Employee(id, firstName, lastName, country, age);
+                    list.add(employee);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> list = parseXML("data.xml");
+        Main.writeString(Main.listToJson(list));
+    }
+}
